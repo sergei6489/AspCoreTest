bcbb9449735b6ebe057f710aff1174cc13c12d0f
sergey <sergeu64_89@mail.ru>
2016-03-29 18:40:20 +0400

    entity identity

--- src/AspCoreTest/Controllers/HomeController.cs
+++ src/AspCoreTest/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspCoreTest.EF;
+using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Mvc;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,9 +13,24 @@ namespace AspCoreTest.Controllers
     [Route( "[controller]" ), Route( "/" )]
     public class HomeController : Controller
     {
+        ApplicationDBContext context;
+        public HomeController( ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+      
         [Route( "[action]" ), Route( "" )]
         public IActionResult Index()
         {
+            try
+            {
+
+
+                var hh = context.Users.ToList();
+            }catch(Exception ex)
+            {
+
+            }
             return View();
         }
     }
