9789ce880c8a72be2b4dc5d84905360a3b6d8624
sergey <sergeu64_89@mail.ru>
2016-03-29 18:45:13 +0400

    Entity Identity

--- src/AspCoreTest/Startup.cs
+++ src/AspCoreTest/Startup.cs
@@ -25,37 +25,43 @@ namespace AspCoreTest
                 .AddJsonFile( "appsettings.json" )
                 .AddJsonFile( $"appsettings.{env.EnvironmentName}.json", optional: true );
 
-            if( env.IsDevelopment() )
-            {
-                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
-            }
 
             builder.AddEnvironmentVariables();
             Configuration = builder.Build();
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IConfigurationRoot Configuration { get; set; }
+        public void ConfigureServices( IServiceCollection services )
         {
-            //services.TryAdd( ServiceDescriptor.Scoped<IRoleStore<SiteRole>, RoleStore<SiteRole>>() );
-            services.AddMvc();
-            // добавление ApplicationDbContext для взаимодействия с базой данных учетных записей
             services.AddEntityFramework()
-                .AddSqlServer()
-                .AddDbContext<ApplicationDBContext>( options =>
-                     options.UseSqlServer( Configuration["Data:DefaultConnection:ConnectionString"] ) );
+            .AddSqlServer()
+            .AddDbContext<ApplicationDBContext>( options =>
+                 options.UseSqlServer( Configuration["Data:DefaultConnection:ConnectionString"] ) );
 
             // добавление сервисов Idenity
             services.AddIdentity<User, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDBContext>()
                 .AddDefaultTokenProviders();
             services.AddTransient<HttpContextHelper>();
+            
+            services.AddMvc();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure( IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory )
         {
+            try
+            {
+                using( var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>()
+                    .CreateScope() )
+                {
+                    serviceScope.ServiceProvider.GetService<ApplicationDBContext>()
+                         .Database.Migrate();
+                }
+            }
+            catch { }
+            app.UseIdentity();
             app.UseIISPlatformHandler( options => options.AuthenticationDescriptions.Clear() );
             app.UseMvc( routes =>
             {
@@ -67,6 +73,6 @@ namespace AspCoreTest
         }
 
         // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+        public static void Main( string[] args ) => WebApplication.Run<Startup>( args );
     }
 }
